5135ace4772306caa9f43f2507c078e124699086
tamarGitHab <aig.tamar@gmail.com>
2025-06-23 23:19:36 +0300

    Initial Commit

---
+++ OfType/OfType/OfTypecs.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OfType
+{
+    internal class OfTypecs : IOfType
+    {
+        public IEnumerable<T> GetOfType<T>(IEnumerable enumerable)
+        {
+            IEnumerable<T> values = enumerable.OfType<T>();
+            return values;
+        }
+
+        public IEnumerable<TOutput> GetOfType<TSource, TOutput>(IEnumerable<TSource> enumerable)
+        {
+            IEnumerable<TOutput> values = enumerable.OfType<TOutput>();
+            return values;
+        }
+
+        public IEnumerable<TBase> OfBase<TBase, TDerived>(IEnumerable<TDerived> derivedItems) where TDerived : TBase
+        {
+            IEnumerable<TBase> values = derivedItems.OfType<TBase>();
+            return values;
+        }
+    }
+}
